1468ac8557d38d054405f5cd409ddb6ab748450a
LionJun <ljun20160606@gmail.com>
2017-11-19 08:19:34 +0800

    docs: 修改leetcode 3注释

--- algorithms/3. Longest Substring Without Repeating Characters.go
+++ algorithms/3. Longest Substring Without Repeating Characters.go
@@ -10,27 +10,27 @@ package algorithms
 //
 //Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
-// 根据ascii码特性 建立字母表 set
-// start未重复substr起始位置 lenMax最大长度
+// 根据ascii码特性, 建立字母表dict
+// start: 未重复substr起始位置, lenMax: 最大长度
 // 将字母位置放入字母表
-// 字母表中字母位置大于start代表字母重复出现 将start右移至上次出现的位置
+// 字母表中字母位置大于start代表字母重复出现, 将start右移至重复出现的位置
 // 根据start和当前index计算长度
 func lengthOfLongestSubstring(s string) int {
 	start := -1
 	lenMax := 0
-	set := [256]int{}
-	for i := range set {
-		set[i] = -1
+	dict := [256]int{}
+	for i := range dict {
+		dict[i] = -1
 	}
 	for i, r := range s {
-		if v := set[r]; v > start {
+		if v := dict[r]; v > start {
 			start = v
 		}
 		length := i - start
 		if length > lenMax {
 			lenMax = length
 		}
-		set[r] = i
+		dict[r] = i
 	}
 	return lenMax
 }
