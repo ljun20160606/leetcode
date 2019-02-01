0c4f7936cca0f1693d63301ac24cb641d1a00bd1
ljun20160606 <ljun20160606@gmail.com>
2019-02-01 15:50:55 +0800

    refactor: interval_test EndIntervals

--- algorithms/interval_test.go
+++ algorithms/interval_test.go
@@ -5,18 +5,37 @@ import (
 	"testing"
 )
 
-func TestIntervals_Len(t *testing.T) {
+func TestStartIntervals_Len(t *testing.T) {
 	var intervals IntervalsStart
 	assert.Equal(t, 0, intervals.Len())
 }
 
-func TestIntervals_Less(t *testing.T) {
+func TestStartIntervals_Less(t *testing.T) {
 	intervals := IntervalsStart{{0, 1}, {1, 2}}
 	assert.True(t, intervals.Less(0, 1))
 }
 
-func TestIntervals_Swap(t *testing.T) {
+func TestStartIntervals_Swap(t *testing.T) {
 	intervals := IntervalsStart{{0, 1}, {1, 2}}
+	assert.NotPanics(t, func() {
+		intervals.Swap(0, 1)
+		assert.Equal(t, intervals[0], Interval{1, 2})
+		assert.Equal(t, intervals[1], Interval{0, 1})
+	})
+}
+
+func TestEndIntervals_Len(t *testing.T) {
+	var intervals IntervalsEnd
+	assert.Equal(t, 0, intervals.Len())
+}
+
+func TestEndIntervals_Less(t *testing.T) {
+	intervals := IntervalsEnd{{0, 1}, {1, 2}}
+	assert.True(t, intervals.Less(0, 1))
+}
+
+func TestEndIntervals_Swap(t *testing.T) {
+	intervals := IntervalsEnd{{0, 1}, {1, 2}}
 	assert.NotPanics(t, func() {
 		intervals.Swap(0, 1)
 		assert.Equal(t, intervals[0], Interval{1, 2})
